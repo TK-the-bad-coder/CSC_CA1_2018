bda301f91472ff0d8132f8f0da86ae32ce474d8d
TK-The-Bad-Coder <tkit.15@ichat.sp.edu.sg>
2018-06-15 20:09:24 +0800

    Add project files.

---
+++ CSCAssignment2018/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CSCAssignment2018.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CSCAssignment2018/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSCAssignment2018.WebApiApplication" Language="C#" %>
